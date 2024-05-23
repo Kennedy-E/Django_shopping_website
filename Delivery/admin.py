c6c7e6117d0de79f8d25789aa0518a9995154d1c
Kennedy-E <estherkennedy83@gmail.com>
2024-05-23 13:45:44 +0100

    Making Changes

--- Delivery/admin.py
+++ Delivery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Goods
+
+@admin.register(Goods)
+class GoodsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+#admin.site.register(Goods)
+
--- Delivery/models.py
+++ Delivery/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
 class Goods(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField(max_length=1000)
+    description = models.CharField(max_length=1000)
     price = models.FloatField()
     quantity = models.IntegerField(default=0)
-    image = models.ImageField(upload_to='home/')
+    image = models.ImageField(upload_to='static/images')
+    slug = models.SlugField(max_length=250, default=None)
+
+    class Meta:
+        verbose_name_plural = "Goods"
+    def __str__(self):
+        return self.name
+
+
+
 
